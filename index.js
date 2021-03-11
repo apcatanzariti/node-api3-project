07e58ec07e53bd1c824d619cee288f183821508e
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-11 15:55:39 -0500

    commits to be pushed to main

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const express = require('express');
+
 // require your server and launch it
 const server = require('./api/server');
 
-server.listen(5000, () => {
-    console.log('server listening on port 5000')
+server.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
 });
